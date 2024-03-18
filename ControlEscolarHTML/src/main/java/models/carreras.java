a703b0088ade2a38668732c2172782cb5bfd6432
Pau Guillen <paugctrabajo@gmail.com>
2024-03-18 14:24:55 +0100

    Primera versión rama Pau

---
+++ ControlEscolarHTML/src/main/java/models/carreras.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author Mati
+ */
+public class carreras {
+    
+    private String nombre;
+
+    public carreras() {
+    }
+
+    public carreras(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Get the value of nombre
+     *
+     * @return the value of nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @param nombre new value of nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
